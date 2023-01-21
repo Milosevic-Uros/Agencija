ae0199e9baf73d98746cfbf37f5ca154c77f030b
Uros Milosevic <milosevicurosit@gmail.com>
2023-01-21 17:51:16 +0100

    Added model classes

---
+++ Shared/Models/Aranzman.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Aranzman
+    {
+        public int aranzmanId { get; set; }
+        public string naziv { get; set; }
+        public DateTime datumPolaska { get; set; }
+        public DateTime datumPovratka { get; set; }
+        public int lokacijaId { get; set; }
+        public string tipPrevoza { get; set; }
+        public string tipAranzmana { get; set; }
+        public int brojmesta { get; set; }
+        public string opis { get; set; }
+        public decimal cena { get; set; }
+    }
+}
---
+++ Shared/Models/Karta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Karta
+    {
+        public int kartaId { get; set; }
+        public string naziv { get; set; }
+        public DateTime datumPolaska { get; set; }
+        public DateTime datumPovratka { get; set; }
+        public int lokacijaId { get; set; }
+        public string tipPrevoza { get; set; }
+        public string tipAranzmana { get; set; }
+        public int brojmesta { get; set; }
+        public decimal cena { get; set; }
+    }
+}
