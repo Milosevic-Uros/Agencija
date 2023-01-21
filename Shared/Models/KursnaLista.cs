d5a84e935c648f2b75adadfdf00251a6ba2e0256
Uros Milosevic <milosevicurosit@gmail.com>
2023-01-21 17:53:43 +0100

    Added KursnaLista model

---
+++ Shared/Models/KursnaLista.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class KursnaLista
+    {
+        public string valutaId { get; set; }
+        public string oznakaValute { get; set; }
+        public string nazivZemlje { get; set; }
+        public decimal srednjiKurs { get; set; }
+    }
+}
