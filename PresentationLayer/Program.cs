845902cf59774bb332ce335006b98e6021019672
Uros Milosevic <milosevicurosit@gmail.com>
2023-01-25 20:42:01 +0100

    Added dependency injection

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLayer;
+using DataLayer.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Interfaces;
 
 namespace PresentationLayer
 {
@@ -17,6 +21,22 @@ namespace PresentationLayer
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                var login = serviceProvider.GetRequiredService<Form1>();
+                Application.Run(login);
+            }
+        }
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<IAdminBusiness, AdminBusiness>();
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IClientBusiness, ClientBusiness>();
+            services.AddScoped<Form1>();
         }
     }
 }
