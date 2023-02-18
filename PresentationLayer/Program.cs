6178c5dd6112110cb1dfd17a60856fb818a785ab
Uros Milosevic <milosevicurosit@gmail.com>
2023-02-18 01:02:06 +0100

    Fixed program.cs

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using BusinessLayer;
+using DataLayer.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace PresentationLayer
 {
@@ -16,7 +21,22 @@ namespace PresentationLayer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                var login = serviceProvider.GetRequiredService<Login>();
+                Application.Run(login);
+            }
+        }
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<IAdminBusiness, AdminBusiness>();
+            services.AddScoped<IClientRepository, ClientRepository>();
+            services.AddScoped<IClientBusiness, ClientBusiness>();
+            services.AddScoped<Login>();
         }
     }
 }
