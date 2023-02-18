b8197a770613008ec0f3f6e0f8551ca7c8aa4ad5
Milica Sebić <92763213+zzomie@users.noreply.github.com>
2023-02-18 01:02:51 +0100

    ClientDashboard created

---
+++ PresentationLayer/ClientDashboard.cs
@@ -0,0 +1,36 @@
+﻿using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class ClientDashboard : Form
+    {
+        private IClientBusiness clientBusiness;
+        private Client client;
+
+        public ClientDashboard()
+        {
+            InitializeComponent();
+        }
+
+        public ClientDashboard(IClientBusiness _clientBusiness, Client _client)
+        {
+            clientBusiness = _clientBusiness;
+            client = _client;
+        }
+
+        private void ClientDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
