f72881d2881d309e149a146ef77f88f5af79ba46
Milica Sebić <92763213+zzomie@users.noreply.github.com>
2023-02-19 04:30:46 +0100

    Regex amount done

--- PresentationLayer/ExchangeOffice.cs
+++ PresentationLayer/ExchangeOffice.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Net.NetworkInformation;
 using System.Runtime.Serialization;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -53,6 +54,12 @@ namespace PresentationLayer
             if(textBoxAmount.Text == "")
             {
                 MessageBox.Show("Provide amount that you want to convert!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBoxAmount.Focus();
+                return;
+            }
+            else if (!Regex.Match(textBoxAmount.Text, "^[0-9]*$").Success)
+            {
+                MessageBox.Show("Amount field accepts only numbers!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 textBoxAmount.Focus();
                 return;
             }
